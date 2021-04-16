8767f50757b20cb841939e7420f22939bbb588a0
walden <walden@163.com>
2021-04-16 11:37:35 +0800

    pitybone

--- src/main/java/com/oops/walden/controller/WaldenIndexController.java
+++ src/main/java/com/oops/walden/controller/WaldenIndexController.java
@@ -37,7 +37,7 @@ public class WaldenIndexController {
     }
     @RequestMapping("/index")
     public String index(Model model) {
-        model.addAttribute("name", "PityBone");
+        model.addAttribute("name", "嘤嘤嘤");
         model.addAttribute("age", 20);
         model.addAttribute("info", "我是一个爱学习的好青年");
         return "index";
